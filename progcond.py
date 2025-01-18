3f598b51ab8bca9e3406b227caadf736166c2a42
mirak2010 <mirakhmad0101@yahoo.com>
2025-01-18 11:34:16 +0500

    #

---
+++ progcond.py
@@ -0,0 +1,8 @@
+num=int(input("Please enter a number: "))
+if num>15:
+    print("The number is greater than 15")
+elif num<15:
+    print("The number is smaller than 15")
+
+elif num==15:
+    print("The number is equal to 15")
